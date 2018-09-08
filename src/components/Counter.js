fbc0cc1331720edcb1f4db16ab02d1f43f30d530
Ricardo Barbosa <rbrico@gmail.com>
2018-09-08 14:14:00 -0300

    exemplo de action e reducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
+import { increment } from "../redux/index";
 class Counter extends Component {
   render() {
     return (
       <div>
-        <h1>1</h1>
+        <h1>{this.props.counter}</h1>
+        <button onClick={() => this.props.increment()}>increment</button>
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  
+  counter: state.counter
 });
 
 const mapDispatchToProps = {
-  
+  increment
 };
 
 export default connect(
